550cd6a7cdf6f9c02b011679e706e6f89a0949ff
flutter drawer <rowaawor999@gmail.com>
2023-06-07 11:14:21 +0545

    Login With Phone Screen

--- lib/UI/auth/login_with_phonenumber.dart
+++ lib/UI/auth/login_with_phonenumber.dart
@@ -1,3 +1,7 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebaseauth/UI/auth/verify_with_code.dart';
+import 'package:firebaseauth/Utils/utils.dart';
+import 'package:firebaseauth/widgets/round_button.dart';
 import 'package:flutter/material.dart';
 class LoginWithPhoneNumber extends StatefulWidget {
   const LoginWithPhoneNumber({super.key});
@@ -7,19 +11,74 @@ class LoginWithPhoneNumber extends StatefulWidget {
 }
 
 class _LoginWithPhoneNumberState extends State<LoginWithPhoneNumber> {
+  bool loading =false;
+  final phoneNumberController = TextEditingController();
+  final auth=FirebaseAuth.instance;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Login With Phone'),
+        title: Text('Phone'),
         centerTitle: true,
       ),
       body: Column(
         children: [
+          SizedBox(height: 40),
+          Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: Container(
+              height: 80,
+              width: 350,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(30),
+                  border: Border.all(
+                      color: Colors.black
+                  ),
+              ),
+              child: Center(
+                child: Text(' Enter Phone Number',style: TextStyle(fontSize: 24),),
+              ),
 
+
+
+            ),
+          ),
+          SizedBox(height: 25,),
+          Center(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 33),
+              child: TextFormField(
+                controller :phoneNumberController,
+                keyboardType: TextInputType.phone,
+                decoration:InputDecoration(
+                  hintText: '+977 9867032145',
+                    prefixIcon: Icon(Icons.phone),
+
+
+                ) ,
+              ),
+            ),
+          ),
+           SizedBox(height: 50,),
+         Container(
+           width: 130,
+           child: RoundButton(title: 'Login', onTap:(){
+        auth.verifyPhoneNumber(
+            phoneNumber:phoneNumberController.text ,
+            verificationCompleted:(_){} ,
+            verificationFailed:(error){
+              Utils().toastMessage(error.toString());
+            } ,
+            codeSent: (String verificationID, int? token){
+              Navigator.push(context, MaterialPageRoute(builder:(context)=>VerifyCodeScreen(verificationId: verificationID,) ));
+            },
+            codeAutoRetrievalTimeout: (error){
+              Utils().toastMessage(error.toString());
+            });
+           }),
+         )
         ],
-      )
-      ,
+      ),
     );
   }
 }
--- lib/UI/auth/verify_with_code.dart
+++ lib/UI/auth/verify_with_code.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/material.dart';
 class VerifyCodeScreen extends StatefulWidget {
-  const VerifyCodeScreen({super.key});
+  final String verificationId;
+  const VerifyCodeScreen({super.key, required this.verificationId});
 
   @override
   State<VerifyCodeScreen> createState() => _VerifyCodeScreenState();
 }
 
 class _VerifyCodeScreenState extends State<VerifyCodeScreen> {
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
